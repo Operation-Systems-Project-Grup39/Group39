41e774815dcee56ac900030291d7be7e36ac7d32
kocerenes <eneskocerr41@gmail.com>
2021-12-14 15:19:13 +0300

    created lsh_launc, lsh execute, lsh_read_line methods.

--- shell.c
+++ shell.c
@@ -73,3 +73,71 @@ int lsh_exit(char **args) {
   printf("exit\n"); 
 	exit(0); // programın sonlanması için.
 }
+
+// programı başlatmaya yarayan metod
+int lsh_launch(char **args) {
+  pid_t pid;
+  int status;
+  // kabuğu başlatmak için forkladık.
+  pid = fork();
+  if (pid == 0) { // Yavru process
+    if (execvp(args[0], args) == -1) { 
+      fprintf(stderr, "Hata : Komut icra edilemiyor.\n");
+    }
+    exit(EXIT_FAILURE);
+  } else if (pid < 0) {// Forklanamadi.
+    perror("lsh");
+  } else { // fork düzgün çalıştıysa
+    // Ebeveyn process
+    do {
+      waitpid(pid, &status, WUNTRACED); // Ana shell bekletiliyor. Yavru process'in görevini tamamlaması bekleniyor.
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+
+  return 1; // uygulamanın devam edebilmesi için 1 dönmeli.
+}
+
+int lsh_execute(char **args)
+{
+  int i; // döngü değişkeni
+
+  if (args[0] == NULL) { // boş komut girildiyse
+    return 1; // uygulamadan çıkmaz
+  }
+  // tanımladığımız built-in komut sayısı kadar dönecek olan döngü
+  for (i = 0; i < lsh_num_builtins(); i++) {
+    if (strcmp(args[0], builtin_str[i]) == 0) { // shell'den gelen komut ile oluşturduğumuz dizi eşleşiyor mu kontrolü
+      return (*builtin_func[i])(args);
+    }
+  }
+  // shell'in terminate edilmesi durumunda 0 dönecek ve process sonlanacak. 1 dönmesi halinde shell çalışmaya devam edecek.
+  return lsh_launch(args);
+}
+
+// stdin input satırını stdin'den oku.
+
+#define LSH_RL_BUFSIZE 80
+char *lsh_read_line(void)
+{
+  int bufsize = LSH_RL_BUFSIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char) * bufsize);
+  int c;
+
+  if (!buffer) {
+    fprintf(stderr, "lsh: bellek tahsis edilemedi...\n");
+    exit(EXIT_FAILURE);
+  }
+  while (1) {
+    // karakteri oku
+    c = getchar();
+
+    if (c == '\n') {
+      buffer[position] = '\0';
+      return buffer;
+    } else {
+      buffer[position] = c;
+    }
+    position++;
+  }
+}
